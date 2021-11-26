4795ba824de75bace156ad471ff0e35593989e96
Abushek <52022566+Abushek@users.noreply.github.com>
2021-11-26 22:00:11 +0530

    Desktop Draggable Objects

--- BUGame/Assets/Scripts/DragItemScript.cs
+++ BUGame/Assets/Scripts/DragItemScript.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class DragItemScript : MonoBehaviour
 {
-    public Collider2D col
-    [SerializeField] GameObject Target;
+    Collider2D col;
+    public GameObject dragObject;
+    public bool spawnCondition=false;
     void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if(col.gameObject.name==dragObject.name)
+        {
+            spawnCondition=true;
+            col.gameObject.SetActive(false);
+        }
     }
 }
